f03346de291d0fd58684bbb855cfa163da4cfabb
MayankPurohit8 <mayankpurohit888@gmail.com>
2025-02-12 20:21:48 +0530

    dep-commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -10,14 +10,14 @@ const { bookSlots } = require("./controllers/bookSlots");
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://event-management-web-app-theta.vercel.app/",
     credentials: true,
   })
 );
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "https://event-management-web-app-theta.vercel.app/",
     credentials: true,
   },
 });
