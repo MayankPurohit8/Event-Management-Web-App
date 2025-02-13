5c3aa353e43b2ee6f41a1435fee6f0bc8d0c9fff
MayankPurohit8 <mayankpurohit888@gmail.com>
2025-02-13 19:12:29 +0530

    dep-commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -9,10 +9,14 @@ const eventRouter = require("./routes/eventRoutes");
 const { bookSlots } = require("./controllers/bookSlots");
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(cors());
+
+app.get("/", function (req, res) {
+  res.send("backend is working correctly");
+});
 app.use("/user", userRouter);
 app.use("/events", eventRouter);
 
